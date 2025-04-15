1f9609b36390f54bcdbeb6dfac03a6edb853d13e
julia-daniluski <julia.o.daniluski@gmail.com>
2025-04-15 08:47:17 -0300

    começo

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+// back-end
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Locadora de veículos</title>
+    <!-- Link do bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Link dos ícones -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- Link para CSS -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
