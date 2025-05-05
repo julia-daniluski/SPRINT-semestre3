399f9065012f1912fe0f639c858e495bed10f8f4
Julia Zanirato <juliazaniratoo@gmail.com>
2025-05-05 15:26:35 -0300

    mudança de valores

--- models/Cenario.php
+++ models/Cenario.php
@@ -5,26 +5,26 @@ use Interfaces\Locavel;
 
 // Classe que representa um carro
 
-class Cenario extends Imoveis implements Locavel {
+class Estudio extends Imoveis implements Locavel {
 
     public function calcularAluguel(int $dias): float {
-        return $dias * DIARIA_CENARIO;
+        return $dias * DIARIA_ESTUDIO;
     }
 
     public function alugar(): string {
         if ($this->disponivel){
             $this->disponivel = false;
-            return "Cenário '{$this->nome}' alugada com sucesso!";
+            return "Estúdio '{$this->nome}' alugada com sucesso!";
         }
-        return "Cenário '{$this->nome}' não está disponível.";
+        return "Estúdio '{$this->nome}' não está disponível.";
     }
 
     public function devolver(): string {
         if (!$this->disponivel){
             $this->disponivel = true;
-            return "Cenário '{$this->nome}' devolvida com sucesso!";
+            return "Estúdio '{$this->nome}' devolvida com sucesso!";
         }
-        return "Cenário '{$this->nome}' está disponível.";
+        return "Estúdio '{$this->nome}' está disponível.";
     }
 
 }
