5652603adbe5e98ee85a87d3846b478c883c7281
Julia Zanirato <juliazaniratoo@gmail.com>
2025-05-05 13:23:55 -0300

    php

---
+++ models/Carro.php
@@ -0,0 +1,33 @@
+<?php
+namespace Models;
+// limita espaços o namespace
+use Interfaces\Locavel;
+
+// classe que representa um carro
+
+class Carro extends Veiculo implements Locavel {
+
+    // cria a funçao de calcular aluguel / float = dinheiro
+    public function calcularAluguel(int $dias): float {
+        return $dias * DIARIA_CARRO;
+        // VARIAVEL QUE RECEBEU $DIAS VEZES A DIARIA
+    }
+
+    // funçao de alugar
+    public function alugar(): string {
+        if ($this->disponivel){
+            $this->disponivel = false;
+            return "Carro '{$this->modelo}' alugado com sucesso!";
+        }
+        return "Carro '{$this->modelo}' nâo está disponivel.";
+    }
+    
+    // funçao de devolver != para dizer que está negando 
+    public function devolver(): string {
+        if (!$this->disponivel){
+            $this->disponivel = true;
+            return "Carro '{$this->modelo}' devolvido com sucesso!";
+        }
+        return "Carro '{$this->modelo}' já está disponivel para alugar.";
+    }   
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'locadora/veiculos',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3a63b4e8385bc9e14290b03a5bf54060ca034307',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'locadora/veiculos' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3a63b4e8385bc9e14290b03a5bf54060ca034307',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
