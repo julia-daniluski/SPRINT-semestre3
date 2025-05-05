c2396a4577406a1ac6968b4efd7ae18c2b482f12
julia-daniluski <julia.o.daniluski@gmail.com>
2025-05-05 15:10:32 -0300

    ok

--- config/config.php
+++ config/config.php
@@ -2,7 +2,9 @@
 
 // Arquivo de configuração com as constantes do sistema 
 
-define('ARQUIVO_JSON', __DIR__ . '/../data/veiculos.json');
+define('ARQUIVO_JSON', __DIR__ . '/../data/imoveis.json');
 define('ARQUIVO_USUARIOS', __DIR__ . '/../data/usuarios.json');
-define('DIARIA_CARRO', 200.00);
-define('DIARIA_MOTO', 100.00);
+define('DIARIA_HOTEL', 500.00);
+define('DIARIA_CASA', 400.00);
+define('DIARIA_QUARTO', 200.00);
+define('DIARIA_ESTUDIO', 100.00);
