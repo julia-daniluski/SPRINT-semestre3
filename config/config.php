c4323ad56113bf234b286660e1466ad6c266a69a
julia-daniluski <julia.o.daniluski@gmail.com>
2025-05-05 15:20:33 -0300

    ok

--- config/config.php
+++ config/config.php
@@ -4,7 +4,6 @@
 
 define('ARQUIVO_JSON', __DIR__ . '/../data/imoveis.json');
 define('ARQUIVO_USUARIOS', __DIR__ . '/../data/usuarios.json');
-define('DIARIA_HOTEL', 500.00);
 define('DIARIA_CASA', 400.00);
-define('DIARIA_QUARTO', 200.00);
+define('DIARIA_QUARTO', 500.00);
 define('DIARIA_ESTUDIO', 100.00);
